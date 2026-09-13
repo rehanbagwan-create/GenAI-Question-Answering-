5fc854854c0c571acc9a21f458b433dd4f62c897
rehanbagwan-create <rehanbagwan8000@gmail.com>
2026-09-14 01:30:25 +0530

    Initial commit  - GenAI Q&A Assistant

---
+++ apps/1_qna_bot.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st 
+
+llm = ChatGoogleGenerativeAI (model="gemini-2.5-flash-lite")
+
+st.title ("🤖 GenAI Q&A Assistant")
+st.markdown (
+    """ **Your intelligent AI assistant for questions, answers, and learning.**
+    
+        Ask a question below and let GenAI provide you with a clear and helpful answer.  
+        Built with **LangChain** and **Streamlit**."""
+)
+
+
+if "message"  not in st.session_state :
+    st.session_state.message =[]
+
+
+for message in st.session_state.message :
+    role =message ["role"]
+    content =message ["content"]
+    st.chat_message(role).markdown(content)
+
+
+
+
+question = st.chat_input("Ask Anything ?")
+
+if question:
+    st.session_state.message.append ({"role": "user", "content":question})
+    st.chat_message ("user").markdown(question)
+    result = llm.invoke(question)
+    st.chat_message("ai").markdown(result.content)
+    st.session_state.message.append({"role":"ai","content": result.content}) 
+
+
